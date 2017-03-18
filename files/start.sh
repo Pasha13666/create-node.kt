c43fa2bae91bd0dfb90f6b481ad30f037b6e4068
Pasha13666 <Pasha13666@users.noreply.github.com>
2017-03-18 10:13:49 +0300

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -8,7 +8,7 @@ fi
 
 if [ "$1" = --compile ];then
     shift
-    kotlinc-jvm -cp "build`find lib '(' -name '*.jar' -o -type d -not -samefile lib ')' -printf ':%p'`" -d build `find src -name '*.kt -printf '%p '` "$@"
+    kotlinc-jvm -cp "build`find lib '(' -name '*.jar' -o -type d -not -samefile lib ')' -printf ':%p'`" -d build `find src -name '*.kt' -printf '%p '` "$@"
 else
     exec "$java" -cp "build`find lib '(' -name '*.jar' -o -type d -not -samefile lib ')' -printf ':%p'`" "$@"
 fi
